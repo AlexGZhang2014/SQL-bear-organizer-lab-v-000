4feea0906ad1067942692545e445a42a760bde32
Alex Zhang <alexgzhang92@gmail.com>
2018-06-11 01:05:20 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, 
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Nicole", 27, "F", "green", "happy", 1)
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Jide", 27, "M", "black", "jovial", 1)
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Ryan", 27, "M", "orange", "indifferent", 1)
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Alex", 26, "M", "blue", "serious", 1)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, NULL, 44, "M", "white", "???", 1)
