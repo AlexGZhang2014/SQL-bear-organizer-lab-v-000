047a6883571c05715a93392aa307ed95ec2d9ecd
Alex Zhang <alexgzhang92@gmail.com>
2018-06-11 00:48:30 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,17 @@
 CREATE TABLE bears (
   id INTEGER PRIMARY KEY,
-  
-  
+  name TEXT,
+  age INTEGER,
+  gender CHAR(1),
+  color TEXT,
+  temperament TEXT
+  alive BOOLEAN
 );
+
+#id	integer
+name	text
+age	integer
+gender	char(1)(The choices would be "M" or "F")
+color	text
+temperament	text
+alive	boolean
